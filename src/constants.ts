ac742feed4c2ce105904aec30927987a5e2b4cee
peeranat-dan <ppeeranat.d@gmail.com>
2023-08-13 14:06:35 +0700

    💬 add imageUrl

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,33 @@
 export const PROJECTS: Project[] = [
+  // {
+  //   name: 'Telecare',
+  //   description:
+  //     'A mini telemedicine platform for small to medium hospitals to schedule appointments for their patients and doctors, enabling communication between them.',
+  //   stacks: ['Nuxt', 'Node', 'Firebase'],
+  // },
   {
-    name: 'Telecare',
+    name: 'Pomoration',
     description:
-      'A mini telemedicine platform for small to medium hospitals to schedule appointments for their patients and doctors, enabling communication between them.',
-    stacks: ['Nuxt', 'Node', 'Firebase'],
+      'A productivity web application that helps users focus on their work with the Pomodoro technique.',
+    stacks: ['React', 'Tailwind', 'Supabase'],
+    url: 'https://pomoration.ninprd.com',
+    imageUrl: '/projects/pomoration-screenshot.webp',
+  },
+  {
+    name: 'Photographer Profile',
+    description:
+      'A photographer profile website using Astro, React, and Tailwind CSS to showcase and display my pricing and availability.',
+    stacks: ['Astro', 'React', 'Tailwind'],
+    url: 'https://nprdphoto.ninprd.com',
+    imageUrl: '/projects/nprdphoto-screenshot.webp',
+  },
+  {
+    name: 'Blog',
+    description:
+      'A blog website using Astro, React, and Tailwind CSS to write about my thoughts, experiences ,and technology.',
+    stacks: ['Astro', 'React', 'Tailwind'],
+    url: 'https://blog.ninprd.com',
+    imageUrl: '/projects/blog-screenshot.webp',
   },
   {
     name: 'Portfolio',
@@ -12,27 +36,7 @@ export const PROJECTS: Project[] = [
     stacks: ['Astro', 'React', 'Tailwind'],
     at: 'True Digital Group',
     url: 'https://portfolio.ninprd.com',
-  },
-  {
-    name: 'Pomoration',
-    description:
-      'A productivity web application that helps users focus on their work with the Pomodoro technique.',
-    stacks: ['React', 'Tailwind', 'Supabase'],
-    url: 'https://pomoration.ninprd.com',
-  },
-  {
-    name: 'Photographer Profile',
-    description:
-      'A photographer profile website using Astro, React, and Tailwind CSS to showcase and display my pricing and availability.',
-    stacks: ['Astro', 'React', 'Tailwind'],
-    url: 'https://nprdphoto.ninprd.com',
-  },
-  {
-    name: 'Blog',
-    description:
-      'A blog website using Astro, React, and Tailwind CSS to write about my thoughts, experiences ,and technology.',
-    stacks: ['Astro', 'React', 'Tailwind'],
-    url: 'https://blog.ninprd.com',
+    imageUrl: '/projects/portfolio-screenshot.webp',
   },
 ];
 
