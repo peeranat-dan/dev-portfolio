a9c06ab8a4253a19a127f3ee882dfe0165266e0c
peeranat-dan <ppeeranat.d@gmail.com>
2024-03-30 11:29:50 +0700

    🔧 add details about wordpress in blog

--- src/constants.ts
+++ src/constants.ts
@@ -24,8 +24,8 @@ export const PROJECTS: Project[] = [
   {
     name: 'Blog',
     description:
-      'A blog website using Astro, React, and Tailwind CSS with @shadcn/ui to write about my thoughts, experiences ,and technology.',
-    stacks: ['Astro', 'React', 'Tailwind'],
+      'A blog website using Astro, React, and Tailwind CSS with @shadcn/ui to express about my thoughts, experiences ,and technology. WordPress is used as a headless CMS.',
+    stacks: ['Astro', 'React', 'Tailwind', 'WordPress (Headless)'],
     url: 'https://blog.ninprd.com',
     imageUrl: '/projects/blog-screenshot.webp',
   },
